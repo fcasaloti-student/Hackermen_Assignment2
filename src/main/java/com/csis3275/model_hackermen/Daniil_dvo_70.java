54b5d00f60248a6c9b8ffb495355a6d694c2eb2f
denngall <volovikd@student.douglascollege.ca>
2020-10-05 17:04:52 -0700

    Added Daniil model class and the profile picture

---
+++ src/main/java/com/csis3275/model_hackermen/Daniil_dvo_70.java
@@ -0,0 +1,18 @@
+package com.csis3275.model_hackermen;
+/**
+ * 
+ * @author Daniil Volovik 
+ * @version 0.1
+ *
+ */
+public class Daniil_dvo_70 extends GroupMember_hackermen {
+
+	public Daniil_dvo_70(String name, String photo) {
+		super(name, photo);
+	}
+	
+	public Daniil_dvo_70() {
+		super("Daniil", "./static/images/Daniil.jpg");
+	}
+
+}
