14a278f7a5dd2ce0189ac6b7e6d70713953b0e54
fcasaloti-student <casalotisilvaf@student.douglascollege.ca>
2020-10-05 17:04:20 -0700

    Created the model class with full name and pointing the photo location. Uploaded the photo to images folder.

---
+++ src/main/java/com/csis3275/model_hackermen/Fernando_fsi_68.java
@@ -0,0 +1,13 @@
+package com.csis3275.model_hackermen;
+
+public class Fernando_fsi_68 extends GroupMember_hackermen{
+	
+	public Fernando_fsi_68(String name, String photo) {
+		super(name, photo);
+	}
+	
+	public Fernando_fsi_68() {
+		super("Fernando Casaloti Silva", "./static/images/Fernando.png");
+	}
+
+}
